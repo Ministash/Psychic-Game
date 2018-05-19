0a274042c8b1a42c2c4e69cbc8ef2630bf2bf2ec
Bob Ross <ministash14@gmail.com>
2018-05-18 20:30:33 -0700

    messing around with different options for the main letter reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,23 +5,23 @@ var userChoiceArray = [];
 const winning = document.getElementById("winning");
 const guessesLeft = document.getElementById("guessNumber");
 const youLost = document.getElementById("superLoser");
+var computerChoice = "a";
 
 
+function computerChoiceFuncion(){
+var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
-function lettersFunction(){
-    var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-
-var computerChoiceTwo = letters[Math.floor(Math.random() * letters.length)];
-return computerChoiceTwo;
+var returnLetters = letters[Math.floor(Math.random() * letters.length)];
+return returnLetters
 }
 
-var computerChoice = lettersFunction();
+var computerChoice = computerChoiceFuncion();
 console.log(computerChoice);
 
 
 document.addEventListener('keypress', function(event){
     var userChoice = event.key;
-    userChoiceArray.push(userChoice);
+    userChoiceArray.push(userChoice + " ");
     compare(userChoice, computerChoice);
     shownGuess.innerHTML = "Your Guess So Far: " + userChoiceArray;
 })
@@ -31,18 +31,24 @@ function compare(userChoice, computerChoice){
     if (userChoice === computerChoice) {
         yourWins++;
         winning.innerHTML = "Wins: " + yourWins;
+        guesses = 10;
+        userChoiceArray = [];
+        computerChoice = computerChoiceFuncion();
+        console.log(computerChoice);
     }
     else if((guesses === 0) || (userChoiceArray.length === 10)){
         yourLoses++;
         youLost.innerHTML = "Losses: " + yourLoses;
         guesses = 10;
         userChoiceArray = [];
+        computerChoice = computerChoiceFuncion();
+        console.log(computerChoice);
+        
     }
     else if(userChoice != computerChoice){
         guesses--;
         guessesLeft.innerHTML = "Guesses Left: " + guesses;
     }
 }
-
 
 
