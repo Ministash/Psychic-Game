033635a3d12d3e5e47ca9cc4e440899da2320b0a
Bob Ross <ministash14@gmail.com>
2018-05-18 19:41:18 -0700

    I some how got the game working? Idk

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,34 +1,48 @@
-var computerChoice = Math.random();
-// Why doesn't this always return rock? Or sometimes both items? It feel like I should have to tell the else if 
-// statement to act within (0.34 <= computerChoice <= 0.67)
-if (computerChoice <= 0.33 ){
-    computerChoice = "rock";
+var guesses = 10;
+var yourWins = 0;
+var yourLoses = 0;
+var userChoiceArray = [];
+const winning = document.getElementById("winning");
+const guessesLeft = document.getElementById("guessNumber");
+const youLost = document.getElementById("superLoser");
+
+
+
+function lettersFunction(){
+    var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+var computerChoiceTwo = letters[Math.floor(Math.random() * letters.length)];
+return computerChoiceTwo;
 }
 
-else if (computerChoice <= 0.67) {
-    computerChoice = "paper";
-}
-
-else {
- computerChoice = "scissors"
-}
-    
-
-
-
-
-
-
-
+var computerChoice = lettersFunction();
 console.log(computerChoice);
 
 
+document.addEventListener('keypress', function(event){
+    var userChoice = event.key;
+    userChoiceArray.push(userChoice);
+    compare(userChoice, computerChoice);
+    shownGuess.innerHTML = "Your Guess So Far: " + userChoiceArray;
+})
+
+
+function compare(userChoice, computerChoice){
+    if (userChoice === computerChoice) {
+        yourWins++;
+        winning.innerHTML = "Wins: " + yourWins;
+    }
+    else if((guesses === 0) || (userChoiceArray.length === 10)){
+        yourLoses++;
+        youLost.innerHTML = "Losses: " + yourLoses;
+        guesses = 10;
+        userChoiceArray = [];
+    }
+    else if(userChoice != computerChoice){
+        guesses--;
+        guessesLeft.innerHTML = "Guesses Left: " + guesses;
+    }
+}
 
 
 
-
-
-
-// else if
-
-// else
