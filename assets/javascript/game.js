9958cee771f82b0509ce2163d597460eb4b3454a
Bob Ross <ministash14@gmail.com>
2018-05-19 09:55:32 -0700

    fixed bugs. V1.0

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,47 +7,58 @@ const guessesLeft = document.getElementById("guessNumber");
 const youLost = document.getElementById("superLoser");
 
 
-function computerChoiceFuncion(){
-var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+function computerChoiceFuncion() {
+    var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-var returnLetters = letters[Math.floor(Math.random() * letters.length)];
-return returnLetters
+    var returnLetters = letters[Math.floor(Math.random() * letters.length)];
+    return returnLetters
 }
 
 var computerChoice = computerChoiceFuncion();
-console.log("This is the letter you neec to guess ======>  " + computerChoice);
 
 
-document.addEventListener('keypress', function(event){
+document.addEventListener('keypress', function (event) {
+    // console.log("----------------------");
+    // console.log("A key was pressed!");
+    // console.log("computerChoice", computerChoice);
+    // console.log("----------------------");
     var userChoice = event.key;
     userChoiceArray.push(userChoice + " ");
     compare(userChoice, computerChoice);
     shownGuess.innerHTML = userChoiceArray;
 })
 
+function resetGame() {
+    // stop listening
+    // select a new computer number
+    computerChoice = computerChoiceFuncion();
+    // display user scores
+    winning.innerHTML = yourWins;
+    youLost.innerHTML = yourLoses;
+    // display user guesses
+    guesses = 10;
+    guessesLeft.innerHTML = "Guesses Left: " + guesses;
+    // draw the correct screen areas
+    // start listening
+    console.log("This is the letter you need to guess ======>  " + computerChoice);
+}
 
-function compare(userChoice, computerChoice){
+function compare(userChoice, computerChoice) {
     if (userChoice === computerChoice) {
         yourWins++;
-        winning.innerHTML = yourWins;
-        guesses = 10;
         userChoiceArray = [];
-        computerChoice = computerChoiceFuncion();
-        console.log("This is the letter you neec to guess ======>  " + computerChoice);
+        resetGame();
     }
-    else if((guesses === -1) || (userChoiceArray.length === 11)){
+    else if ((guesses === 0) || (userChoiceArray.length === 10)) {
         yourLoses++;
-        youLost.innerHTML = yourLoses;
-        guesses = 10;
         userChoiceArray = [];
-        computerChoice = computerChoiceFuncion();
-        console.log("This is the letter you neec to guess ======>  " + computerChoice);
-        
+        resetGame();
     }
-    else if(userChoice != computerChoice){
+    else if (userChoice != computerChoice) {
         guesses--;
         guessesLeft.innerHTML = "Guesses Left: " + guesses;
     }
 }
 
 
+resetGame();
